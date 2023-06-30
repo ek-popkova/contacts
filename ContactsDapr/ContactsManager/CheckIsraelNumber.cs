b2cf9f2288eab851616e6433324fc2f419789054
Popkova Ekaterina <111788752+ek-popkova@users.noreply.github.com>
2023-06-30 16:22:16 +0300

    validation added to manager

---
+++ ContactsDapr/ContactsManager/CheckIsraelNumber.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManager
+{
+    public class CheckIsraelNumber : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? phone, ValidationContext validationContext)
+        {
+            var phone_number = phone as string;
+
+            if (string.IsNullOrWhiteSpace(phone_number))
+            {
+                return new ValidationResult("Phone number cannot be empty");
+            }
+
+            if (phone_number.Length < 10 || phone_number.Length > 14)
+            {
+                return new ValidationResult("Phone number shuold contain from 10 to 14 symbols");
+            }
+
+            if (!(phone_number.StartsWith("+972") || !phone_number.StartsWith("0")))
+            {
+                return new ValidationResult("Phone number should begin with +972 or 0");
+            }
+
+            if (((phone_number[0] == '+') && (!phone_number[1..].All(char.IsDigit))) ||
+                ((phone_number[0] != '+') && (!phone_number.All(char.IsDigit))))
+            {
+                return new ValidationResult("Phone number should contain only digits or +");
+            }   
+
+            return ValidationResult.Success;
+        }
+    }
+}
+
--- ContactsDapr/ContactsManager/Models/Contact.cs
+++ ContactsDapr/ContactsManager/Models/Contact.cs
@@ -1,12 +1,14 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ContactsManager.Models
 {
 
     public class Contact
     {
+        [Required(ErrorMessage = "Name cannot be empty")]
         public required string Name { get; set; }
 
+        [CheckIsraelNumber(ErrorMessage = "Phone number you supplied is not a valid israel phone number.")]
         public required string Phone { get; set; }
     }
 }
